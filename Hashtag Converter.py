785f00751253db6c5f902d269ed0d7df0c3d9857
sunzooprince <75158166+sunzooprince@users.noreply.github.com>
2020-12-05 00:06:17 -0500

    Add files via upload

---
+++ Hashtag Converter.py
@@ -0,0 +1,44 @@
+
+# this is how we make a hashtag to text creator
+
+# there are four parts to this
+"""
+text = []
+text = input("Text:\n")
+
+
+half = []
+
+def splitter(string):
+    half = list(string.split(" "))
+
+
+splitter(text)
+
+print(text)
+print(half)
+
+input_string = input("Enter a list elements separated by space ")
+
+print("\n")
+userList = input_string.split()
+"""
+
+# placeholder texte:
+# function to split a string by space and added those numbers to the list
+
+"""
+>>> suits = ["h","c", "d", "s"]
+>>> aces = ["a" + suit for suit in suits]
+>>> aces
+['ah', 'ac', 'ad', 'as']
+"""
+
+
+beginning = input("text:\n")
+broken = beginning.split()
+print(broken)
+final = ["#" + word for word in broken]
+print(final)
+print(*final, sep =" ")
+
